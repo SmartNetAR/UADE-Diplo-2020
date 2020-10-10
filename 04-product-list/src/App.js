acdb70c414823e1ca986543f106bf0b4fbb06f90
Leonardo Casales <leonardo@smartnet.com.ar>
2020-10-09 22:28:58 -0300

    add products

--- 04-product-list/src/App.js
+++ 04-product-list/src/App.js
@@ -8,8 +8,15 @@ class App extends React.Component {
     constructor(prop) {
       super(prop);
       this.state = {
-          showPersona: true,
-          otroEstado: "Estado nuevo"
+          showPersona: false,
+          producto: "",
+          cantidad: 0,
+          productos: [ 
+            { id: 1, nombre: "monitor", cantidad: 100},
+            { id: 2, nombre: "teclado", cantidad: 150}, 
+            { id: 3, nombre: "mouse", cantidad: 4}, 
+            { id: 4, nombre: "impresora", cantidad: 0}
+        ]
       }
     }
 
@@ -19,35 +26,89 @@ class App extends React.Component {
         edad: 33,
         domicilio: "Av. JBJusto"
     };
-    
-    productos = [ 
-        { id: 1, nombre: "monitor", cantidad: 100},
-        { id: 2, nombre: "teclado", cantidad: 150}, 
-        { id: 3, nombre: "mouse", cantidad: 4}, 
-        { id: 4, nombre: "impresora", cantidad: 0}
-    ];
-
 
     handleClick = () => {
-      if ( this.state.showPersona ) {
-        // this.state.showPersona = false;
-        this.setState({ showPersona: false })
-      } else {
-        this.setState({ showPersona: true })
-      }
 
-      this.setState({ otroEstado: "nuevo valor" })
+      // let manzana = "manzana";
+      // let pera = manzana;
+      // manzana = "pera";
+
+      // console.log("1", manzana);
+      // console.log("2", pera);
+
+      // const frutas = ["pera", "naranja"];
+
+      // const newFrutas =  [...frutas, "sandía", ...frutas];
+      // console.log("1- newFrutas", newFrutas);
+
+      // frutas.push("manzana");
+
+      // console.log("2- frutas", frutas);
+      // console.log("3- newFrutas", newFrutas);
+
+      
+
+
+
+      // if ( this.state.showPersona ) {
+      //   // this.state.showPersona = false;
+      //   this.setState({ showPersona: false })
+      // } else {
+      //   this.setState({ showPersona: true })
+      // }
+
+      // this.setState({ otroEstado: "nuevo valor" })
+    }
+
+    handleChange = ( evento ) => {
+      const {name, value} = evento.target ;
+                      //key:  value
+      this.setState({ [name]: value });
+
+    }
+
+    agregarProducto = () => {
+
+        const productoNuevo = {
+           id: this.state.productos.length + 1 ,
+           nombre: this.state.producto,
+           cantidad: parseInt( this.state.cantidad )
+        }
+
+        this.setState({
+          productos: [...this.state.productos, productoNuevo ]
+        })
+
     }
 
     render() {
       return (
         <div className="App">
           <header className="App-header">
-            <button onClick={this.handleClick} >Toggle</button>
-            <ListaProductos
-                listaProductos={this.productos}
+
+            <p>Producto</p>
+            <input
+                type="text"
+                onChange={this.handleChange}
+                name="producto"
+                value={this.state.producto}
             />
-            <p>{this.state.otroEstado}</p>
+
+            <p>Cantidad</p>
+            <input type="number"
+                onChange={this.handleChange} 
+                name="cantidad" 
+                value={this.state.cantidad} 
+            />
+
+            <br></br>
+
+            <button onClick={this.agregarProducto}> Agregar </button>
+            
+            <ListaProductos
+                listaProductos={this.state.productos}
+            />
+            <button onClick={this.handleClick} >Toggle</button>
             <Persona persona={this.persona} isShow={this.state.showPersona}/>
           </header>
         </div>
